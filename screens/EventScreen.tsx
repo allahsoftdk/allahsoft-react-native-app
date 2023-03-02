2b028441a60c7ea307935d2a01b8247e6b522201
Isaac Vorköper <isaaclennart@gmail.com>
2023-03-02 14:46:40 +0100

    event prep

--- screens/EventScreen.tsx
+++ screens/EventScreen.tsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { NativeBaseProvider, Box, Center} from "native-base";
-import { useColorScheme } from "react-native";
+import * as React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import { Box, Center, Container, FlatList, Heading, Stack, Text } from "native-base";
+import { useWindowDimensions } from "react-native";
+import { TabView, SceneMap } from 'react-native-tab-view';
+import axios from "axios";
+import { PrayerTimes } from "../types";
 
-const EventScreen = ({ navigation }: { navigation: any }) => {
-    const colorScheme = useColorScheme();
+export default function EventScreen({ route, navigation }: { route: any, navigation: any }) {
+    const layout = useWindowDimensions();
+    const [alarms, setAlarms] = React.useState<PrayerTimes>(route.params);
+
+    const [index, setIndex] = React.useState(0);
+
+
     return (
-        <NativeBaseProvider>
-            <Center w="100%">
-                <Box safeArea p="2" py="8" w="90%" maxW="290">
-                </Box>
-            </Center>
-        </NativeBaseProvider>
-
-    )
-};
-export default EventScreen;
+        <Center>
+            <FlatList data={null} renderItem={({ item }) => (
+                <Container>
+                    <Box alignItems="center">
+                        <Stack p="3" space={3}>
+                            <Box minW={"250"} rounded="lg" overflow="hidden" borderColor="#165d31" borderWidth="1" _dark={{ borderColor: "coolGray.600", backgroundColor: "gray.700" }} _web={{ shadow: 2, borderWidth: 0 }} _light={{ backgroundColor: "gray.50" }}>
+                                <Stack p="4" space={3}>
+                                    <Stack space={2}>
+                                        <Heading size="md" ml="-1">
+                                            <FontAwesome name="bell" size={20} color="#165d31" />
+                                            <Stack p="1" space={1}></Stack>
+                                            joe
+                                        </Heading>
+                                    </Stack>
+                                    <Text fontSize={"md"} fontWeight="400">joe</Text>
+                                </Stack>
+                            </Box>
+                        </Stack>
+                    </Box>
+                </Container>)}
+            />
+        </Center>
+    );
+}
